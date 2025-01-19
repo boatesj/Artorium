485a7ebf37eff09f5e8417fbdbcbd1b0a4fbbfe1
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-01-19 19:34:04 +0000

    Customised admin

--- artworks/admin.py
+++ artworks/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Artwork, Category
 
 # Register your models here.
-admin.site.register(Artwork)
-admin.site.register(Category)
+
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'artist',
+        'category',
+        'price',
+        'available',
+        'image',
+    )
+
+    ordering = ('title',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Artwork, ArtworkAdmin)
+admin.site.register(Category, CategoryAdmin)
