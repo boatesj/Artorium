b5acd68c469f121c06c46deac8c24ebf43471611
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-01-20 18:03:30 +0000

    Added sorting

--- artworks/views.py
+++ artworks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404 
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Artwork, Category
 
 # Create your views here.
@@ -11,8 +12,23 @@ def all_artworks(request):
     artworks = Artwork.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'title':  # Assuming 'title' is the field to sort by
+                sortkey = 'lower_title'
+                artworks = artworks.annotate(lower_title=Lower('title'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            artworks = artworks.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             artworks = artworks.filter(category__name__in=categories)
@@ -23,14 +39,17 @@ def all_artworks(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('artworks'))
-            
+
             queries = Q(title__icontains=query) | Q(description__icontains=query) | Q(artist__icontains=query)
             artworks = artworks.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'artworks': artworks,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'artworks/artworks.html', context)
