26e721ebd5e5ead9ecd1978115007e5e157a803a
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-01-21 14:01:16 +0000

    Added SKU to the artworks model

--- artworks/models.py
+++ artworks/models.py
@@ -26,6 +26,7 @@ class Artwork(models.Model):
     image = models.ImageField(upload_to='artworks/', null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     available = models.BooleanField(default=True)
+    sku = models.CharField(max_length=20, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
