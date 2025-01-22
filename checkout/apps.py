b3e9560596d330f4e3bf60f52be9458ae608477b
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-01-22 19:17:01 +0000

    Added forms, admin and signals and also added placeholders and styling to improve UX

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'  # Ensures compatibility with newer Django versions
     name = 'checkout'
+
+    def ready(self):
+        """
+        Import and connect signals to their handlers
+        """
+        import checkout.signals
