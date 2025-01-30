e78d28663307a3391150b12a6202c05dc0226a86
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-01-30 15:46:08 +0000

    Fixed custom_storages.py to correctly handle media files

--- custom_storages.py
+++ custom_storages.py
@@ -3,7 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-
+    default_acl = 'public-read'
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    file_overwrite = False  # Prevents overwriting files with the same name
+    default_acl = 'public-read'
