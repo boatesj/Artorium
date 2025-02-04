67433afead6f913903fa62d8368b3d357357ae87
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-02-04 15:11:25 +0000

    Fix NoReverseMatch error by using correct URL namespace and names

--- artworks/urls.py
+++ artworks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'artworks'
+
 urlpatterns = [
     path('', views.all_artworks, name='artworks'),
     path('<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
