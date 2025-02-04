7be6c03c613820f436ce67a7ed8f9b82536030e3
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-02-04 16:03:55 +0000

    NoReverseMAtch Debugging

--- artworks/urls.py
+++ artworks/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'artworks'
-
 urlpatterns = [
-    path('', views.all_artworks, name='artworks'), 
+    path('', views.all_artworks, name='artworks'),  # Main 'artworks' URL pattern
     path('<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
     path('add/', views.add_artwork, name='add_artwork'),
     path('edit/<int:artwork_id>/', views.edit_artwork, name='edit_artwork'),
@@ -12,5 +10,5 @@ urlpatterns = [
     path('add/artist/', views.add_artwork_artist, name='add_artwork_artist'),
     path('manage-portfolio/', views.manage_portfolio, name='manage_portfolio'),
     path('categories/', views.category_list, name='category_list'),
-    path('list/', views.artwork_list, name='artwork_list'),  
+    path('list/', views.artwork_list, name='artwork_list'),  # Renamed to 'list'
 ]
