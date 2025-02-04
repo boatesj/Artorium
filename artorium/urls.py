0b74967364f25f7af8dace79d3d22a5bc57baf0b
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2025-02-04 15:42:57 +0000

    NoReverseMatch Fix

--- artorium/urls.py
+++ artorium/urls.py
@@ -19,11 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from profiles import views as profile_views
 
+namespace='artworks'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('artworks/', include('artworks.urls')),
+    path('artworks/', include('artworks.urls')), 
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),  # Ensure profiles URLs are included
